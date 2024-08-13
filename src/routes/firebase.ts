85679b9f10c921ee9c3d96c9683a1ff73242ea30
Lee Doyun <maxlee0125@naver.com>
2024-08-13 16:24:02 +0900

    firebase launched

---
+++ src/routes/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBzaQZsVd3F7vcs6RA4uiEmcvHbJb1z9mg",
+  authDomain: "nwitterclass.firebaseapp.com",
+  projectId: "nwitterclass",
+  storageBucket: "nwitterclass.appspot.com",
+  messagingSenderId: "373363265339",
+  appId: "1:373363265339:web:b76fc9885028006f84311e"
+};
+const app = initializeApp(firebaseConfig);
